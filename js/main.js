9198b9612cd77eaf0bdf14100fb67801660d3a4a
tuld <tuld@vieted.com>
2021-02-18 16:06:11 +0700

    add repo

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
+const contentList = $('.navContent_content_children2_button_text')
+    contentList.forEach(function(content, index) {
+        content.onclick
+    });
